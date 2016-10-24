08357afd8be482e497938cf5cf59bb5ba35805f4
Igor <igor@lingvist.io>
2016-10-24 11:07:17 +0300

    Initial commit. NodeBB Forum SignIn/Register using user credentials from lingvist.com

---
+++ library.js
@@ -0,0 +1,216 @@
+(function (module) {
+    "use strict";
+    var InternalOAuthError = module.require('passport-oauth').InternalOAuthError;
+
+    var User = module.parent.require('./user'),
+        Groups = module.parent.require('./groups'),
+        utils = module.parent.require('../public/src/utils'),
+        meta = module.parent.require('./meta'),
+        db = module.parent.require('../src/database'),
+        passport = module.parent.require('passport'),
+        fs = module.parent.require('fs'),
+        path = module.parent.require('path'),
+        nconf = module.parent.require('nconf'),
+        winston = module.parent.require('winston'),
+        jws = module.parent.require('jws'),
+        async = module.parent.require('async');
+
+    var authenticationController = module.parent.require('./controllers/authentication');
+
+    var constants = Object.freeze({
+            admin: {
+                route: '/plugins/sso-lingvist',
+                icon: 'icon-lingvist'
+            },
+
+            name: 'lingvist'	// Something unique to your OAuth provider in lowercase, like "github", or "nodebb"
+        }),
+        OAuth = {}, passportOAuth, opts;
+
+    OAuth.init = function (data, callback) {
+        function render(req, res, next) {
+            res.render('admin/plugins/sso-lingvist', {});
+        }
+
+        data.router.get('/admin/plugins/sso-lingvist', data.middleware.admin.buildHeader, render);
+        data.router.get('/api/admin/plugins/sso-lingvist', render);
+
+        callback();
+    };
+
+    OAuth.getStrategy = function (strategies, callback) {
+
+        meta.settings.get('sso-lingvist', function (err, settings) {
+            if (!err && settings.id && settings.secret && settings.token_url && settings.authorization_url) {
+
+                passportOAuth = require('passport-oauth')['OAuth2Strategy'];
+
+                // OAuth 2 options
+                opts = {};
+                opts.clientID = settings.id;
+                opts.clientSecret = settings.secret;
+                opts.authorizationURL = settings.authorization_url;
+                opts.tokenURL = settings.token_url;
+                opts.callbackURL = nconf.get('url') + '/auth/' + constants.name + '/callback';
+
+                passportOAuth.Strategy.prototype.userProfile = function (accessToken, done) {
+                    var decoded = jws.decode(accessToken);
+
+                    if (decoded === null || decoded.payload === undefined) {
+                        return done(new InternalOAuthError('Do not understand token content'));
+                    }
+
+                    try {
+                        var json = decoded.payload;
+                        OAuth.parseUserReturn(json, function (err, profile) {
+                            if (err) return done(err);
+                            profile.provider = constants.name;
+
+                            done(null, profile);
+                        });
+                    } catch (e) {
+                        done(e);
+                    }
+                };
+
+                opts.passReqToCallback = true;
+
+                passport.use(constants.name, new passportOAuth(opts, function (req, token, secret, profile, done) {
+                    OAuth.login({
+                        oAuthid: profile.id,
+                        handle: profile.displayName,
+                        email: profile.emails[0].value,
+                        isAdmin: profile.isAdmin
+                    }, function (err, user) {
+                        if (err) {
+                            return done(err);
+                        }
+
+                        authenticationController.onSuccessfulLogin(req, user.uid);
+                        done(null, user);
+                    });
+                }));
+
+                strategies.push({
+                    name: constants.name,
+                    url: '/auth/' + constants.name,
+                    callbackURL: '/auth/' + constants.name + '/callback',
+                    icon: constants.admin.icon,
+                    scope: (constants.scope || '').split(',')
+                });
+
+                callback(null, strategies);
+            } else {
+                callback(new Error('OAuth Configuration is invalid'));
+            }
+        });
+    };
+
+    OAuth.parseUserReturn = function (data, callback) {
+
+        var profile = {};
+        profile.id = data.id;
+        profile.displayName = data.displayName;
+        profile.emails = [{value: data.email}];
+
+        if (!utils.isUserNameValid(profile.displayName)) {
+            profile.displayName = utils.slugify(profile.displayName, true);
+        }
+
+        // Do you want to automatically make somebody an admin? This line might help you do that...
+        // profile.isAdmin = data.isAdmin ? true : false;
+
+        callback(null, profile);
+    };
+
+    OAuth.login = function (payload, callback) {
+        OAuth.getUidByOAuthid(payload.oAuthid, function (err, uid) {
+            if (err) {
+                return callback(err);
+            }
+
+            if (uid !== null) {
+                // Existing User
+                callback(null, {
+                    uid: uid
+                });
+            } else {
+                // New User
+                var success = function (uid) {
+                    // Save provider-specific information to the user
+                    User.setUserField(uid, constants.name + 'Id', payload.oAuthid);
+                    db.setObjectField(constants.name + 'Id:uid', payload.oAuthid, uid);
+
+                    if (payload.isAdmin) {
+                        Groups.join('administrators', uid, function (err) {
+                            callback(null, {
+                                uid: uid
+                            });
+                        });
+                    } else {
+                        callback(null, {
+                            uid: uid
+                        });
+                    }
+                };
+
+                User.getUidByEmail(payload.email, function (err, uid) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    if (!uid) {
+                        User.create({
+                            username: payload.handle,
+                            email: payload.email
+                        }, function (err, uid) {
+                            if (err) {
+                                return callback(err);
+                            }
+
+                            success(uid);
+                        });
+                    } else {
+                        success(uid); // Existing account -- merge
+                    }
+                });
+            }
+        });
+    };
+
+    OAuth.getUidByOAuthid = function (oAuthid, callback) {
+        db.getObjectField(constants.name + 'Id:uid', oAuthid, function (err, uid) {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, uid);
+        });
+    };
+
+    OAuth.deleteUserData = function (uid, callback) {
+        async.waterfall([
+            async.apply(User.getUserField, uid, constants.name + 'Id'),
+            function (oAuthIdToDelete, next) {
+                db.deleteObjectField(constants.name + 'Id:uid', oAuthIdToDelete, next);
+            }
+        ], function (err) {
+            if (err) {
+                winston.error('[sso-oauth] Could not remove OAuthId data for uid ' + uid + '. Error: ' + err);
+                return callback(err);
+            }
+            callback(null, uid);
+        });
+    };
+
+    OAuth.addMenuItem = function (custom_header, callback) {
+        custom_header.authentication.push({
+            "route": constants.admin.route,
+            "icon": constants.admin.icon,
+            "name": constants.name
+        });
+
+        callback(null, custom_header);
+    };
+
+    module.exports = OAuth;
+}(module));
---
+++ templates/admin/plugins/sso-lingvist.tpl
@@ -0,0 +1,62 @@
+<div class="row">
+	<div class="col-lg-9">
+		<div class="panel panel-default">
+			<div class="panel-heading">Lingvist Social Authentication</div>
+			<div class="panel-body">
+				<form role="form" class="sso-lingvist-settings">
+					<div class="form-group">
+						<label for="id">OAuth ID</label>
+						<input type="text" id="id" name="id" title="OAuth ID" class="form-control" placeholder="OAuth ID"><br />
+					</div>
+					<div class="form-group">
+						<label for="secret">OAuth Password</label>
+						<input type="text" id="secret" name="secret" title="OAuth Password" class="form-control" placeholder="OAuth Password">
+					</div>
+
+					<div class="form-group">
+						<label for="authorization_url">Authorization URL</label>
+						<input type="text" id="authorization_url" name="authorization_url" title="OAuth Authorization URL" class="form-control" placeholder="OAuth Authorization URL">
+					</div>
+					<div class="form-group">
+						<label for="token_url">Authorization URL</label>
+						<input type="text" id="token_url" name="token_url" title="OAuth Token URL" class="form-control" placeholder="OAuth Token URL">
+					</div>
+
+					<p class="help-block">
+						The appropriate "Redirect URI" is your NodeBB's URL with `/auth/lingvist/callback` appended to it.
+					</p>
+					<div class="checkbox">
+						<label for="showSiteTitle" class="mdl-switch mdl-js-switch mdl-js-ripple-effect">
+							<input type="checkbox" class="mdl-switch__input" id="showSiteTitle" name="autoconfirm" />
+							<span class="mdl-switch__label">Skip email verification for people who register using SSO?</span>
+						</label>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
+<button id="save" class="floating-button mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+	<i class="material-icons">save</i>
+</button>
+
+<script>
+	require(['settings'], function(Settings) {
+		Settings.load('sso-lingvist', $('.sso-lingvist-settings'));
+
+		$('#save').on('click', function() {
+			Settings.save('sso-lingvist', $('.sso-lingvist-settings'), function() {
+				app.alert({
+					type: 'success',
+					alert_id: 'sso-lingvist-saved',
+					title: 'Settings Saved',
+					message: 'Please reload your NodeBB to apply these settings',
+					clickfn: function() {
+						socket.emit('admin.reload');
+					}
+				})
+			});
+		});
+	});
+</script>
